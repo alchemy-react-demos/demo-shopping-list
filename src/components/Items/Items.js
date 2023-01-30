3c2034b365f661d7aee0a3f976709cb1710d459a
Julie Nisbet <jmnisbet@gmail.com>
2023-01-30 10:51:56 -0800

    wiring up header

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { createListItem } from '../../services/items';
 import ItemsList from './ItemsList';
 import ItemForm from './ItemForm';
+import { UserContext } from '../../context/UserContext';
+import { Redirect } from 'react-router-dom';
 
 export default function Items() {
   // TODO -- redirect the user back to auth if there is not a current user
-
+  const { user } = useContext(UserContext);
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
   return (
     <div className="container is-max-desktop">
       <div className="box m-5">
