05a5ac4623d8d7b073fd2161d84d509835fe445d
Julie Nisbet <jmnisbet@gmail.com>
2023-01-30 09:44:30 -0800

    adding the UserProvider and UserContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,13 @@
+const { createContext, useState } = require('react');
+const { getUser } = require('../services/auth');
+
 // TODO -- create the user provider!
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export { UserProvider, UserContext };
