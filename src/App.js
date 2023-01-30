2d6a442e037cca32ca884d72b9114d679a8b8d1e
Julie Nisbet <jmnisbet@gmail.com>
2023-01-30 09:52:51 -0800

    add redirects to home route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header';
 import Items from './components/Items/Items';
 import backgroundImage from './supermarket.jpeg';
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
 
 function App() {
+  const { user } = useContext(UserContext);
   return (
     <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="/items" component={Items} />
-        <Route exact path="/"></Route>
+        <Route exact path="/">
+          <>
+            {user && <Redirect to="/items" />}
+            {!user && <Redirect to="/auth/sign-in" />}
+          </>
+        </Route>
       </Switch>
     </div>
   );
